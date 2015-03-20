50a4bc28b320ed385061cb31fe2edebbeab9a22f
Flavio Barros <flaviomargarito@gmail.com>
2015-03-20 12:10:57 -0300

    Incluído recurso de passar variável para o javascript

--- app/pages/views.py
+++ app/pages/views.py
@@ -12,7 +12,8 @@ from app.app_and_db import app
 # The Home page is accessible to anyone
 @app.route('/')
 def home_page():
-    return render_template('pages/home_page.html')
+    fen = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+    return render_template('pages/home_page.html', fen=fen)
 
 # The Member page is accessible to authenticated users (users that have logged in)
 @app.route('/member')
