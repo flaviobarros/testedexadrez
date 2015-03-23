e33099ccb39680dce87a2e90f46d54e4f2f9f6c2
Flavio Barros <flaviomargarito@gmail.com>
2015-03-23 16:18:38 -0300

    Incluido formulário na página home e contact

--- app/pages/views.py
+++ app/pages/views.py
@@ -3,17 +3,17 @@
 # Authors: Ling Thio <ling.thio@gmail.com>
 
 
-from flask import render_template
+from flask import Flask, render_template, request 
 from flask_user import login_required, roles_required
-
+from app.pages.forms import ContactForm
 from app.app_and_db import app
 
 
 # The Home page is accessible to anyone
 @app.route('/')
 def home_page():
-    fen = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
-    return render_template('pages/home_page.html', fen=fen)
+    form = ContactForm()
+    return render_template('pages/home_page.html', form=form)
 
 # The Member page is accessible to authenticated users (users that have logged in)
 @app.route('/member')
@@ -26,3 +26,14 @@ def member_page():
 @roles_required('admin')    # Limits access to users with the 'admin' role
 def admin_page():
     return render_template('pages/admin_page.html')
+
+# A contact page to test form 
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        return 'Form posted.'
+
+    elif request.method == 'GET':
+        return render_template('pages/contact.html', form=form)
